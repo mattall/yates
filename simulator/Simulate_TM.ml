392de38f4dc3b254c66ad1557fb9dfc367930f52
mattall <mhhall92@gmail.com>
2019-05-20 13:21:45 -0700

    added debug message

--- simulator/Simulate_TM.ml
+++ simulator/Simulate_TM.ml
@@ -545,6 +545,8 @@ let simulate_tm (start_scheme:scheme)
                   
                   Printf.printf "%s: %f \n%!" (string_of_edge topo e) (current_edge_capacity);
 
+                  Printf.print count_paths_through_edge (scheme);
+
                   (* total ingress traffic on link *)
                   let demand_on_link = List.fold_left in_queue_edge
                       ~init:0.0
